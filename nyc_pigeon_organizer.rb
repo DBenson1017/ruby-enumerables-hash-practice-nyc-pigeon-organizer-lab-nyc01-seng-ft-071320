e9e1084c12c55f38302f65db01553319aac06f89
Devin Benson <devinbenson@gmail.com>
2020-07-04 16:37:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,32 +1,4 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final_hash = {}
-  data.each do |top_three, hash_value|
-    hash_value.each do |inner_key, inner_array|
-     inner_array.each do |item|
-       if final_hash[item] == nil 
-         final_hash[item] = {} 
-         final_hash[item][top_three] = []
-         binding.pry
-      else  
-     final_hash[item][top_three] = []
-      end
-    end
-  end
-end
-new_hash.each do |names, values|
-  values.each do |inner_key, array|
-    data.each |top_three, values|
-    values.each do | value, array|
-      array.each |element|
-        if element == itme && hashvalue == top_three 
-          new_hash[item][hashvalue] << value.to_s 
-        end 
-      end 
-    end 
-  end 
-end 
-end 
-new_hash
-end 
+  final.results = 
