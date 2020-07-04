d31c4059ba50cdb36e6bc0ac4ba747e757b303ab
Devin Benson <devinbenson@gmail.com>
2020-07-04 16:12:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,31 +18,3 @@ new_hash
 end 
 
 nyc_pigeon_organizer
-
-
-=begin data.each do |attribute, value|
-  #itterates over the hash and looks for gender, then adds the names from 
-  if attribute == :gender 
-    
-    if i < :male.length then 
-      :male[i] = name 
-      organized_data << name 
-    end 
-    i += 1 
-  else if attribute == :gender && value == :female 
-     i = 0 
-    if i < :female.length then 
-      :female[i] = name 
-      organized_data << name 
-      binding.pry
-    end 
-    i += 1 
-  end 
-  end 
- end 
- organized_data
- end
- 
- nyc_pigeon_organizer(pigeon_data)
-
-=end 
